8cf6b9a415448221e6fbbc45708fc8e44cba6f34
Kendall Rowe <kendall.rowe312gmail.com>
2019-06-30 19:32:04 +0000

    Created constructor function to generate a board array or space objects

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
+// Constructor function constaining closure function to generate spaces on the board
+function BoardSpace() {
+  this.createSpace = function(alphaIndex, row) {
+    const alphabetOrder = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+  
+    return function() {
+      if (alphaIndex === 9) {
+        row++;
+        alphaIndex = -1;
+      }
+      alphaIndex++;
+
+      return alphabetOrder[alphaIndex] + row;
+    }
+  },
+  this.ship = {}
+}
+
+// Tiles length of each ship type, start and end coordinates of ship, boolean of whether it has been sunk
+const shipStatus = {
+  carrier: [[], 5],
+  battleship: [[], 4],
+  cruiser: [[],3],
+  submarine: [[], 3],
+  destroyer: [[], 2]
+}
+
+function Ship(shipType, spaces) {
+  this.player = "",
+  this.spaces = spaces,
+  this.numberOfHits = 0;
+  this.type = shipType,
+  this.className
+}
+
 // Create an empty board array for a player storing null ship type and shot fired within each cell
 const generateBoard = function() {
   let boardArray = new Array(10);
-  
+  const firstSpace = new BoardSpace();
+  const createSpace = firstSpace.createSpace(-1, 1);
+
   // Iterate through each row of array
   for (let row = 0; row < 10; row++) {
     // Create new array for each row
     boardArray[row] = new Array(10);
     for (let column = 0; column < 10; column++) {
       // Create array for each cell storing ship type and if a shot has been fired array
-      boardArray[row][column] = ["",false];
+      boardArray[row][column] = new BoardSpace
+      boardArray[row][column].coordinate = createSpace();
     }
   }
   return boardArray;
 };
+
+const test = generateBoard();
+
+console.log(test);
 
 const placeShipOnBoard = function(currentBoard, vertical, shipType, shipStart) {
   // Check whether a ship is already in the specified location
@@ -23,14 +65,6 @@ const placeShipOnBoard = function(currentBoard, vertical, shipType, shipStart) {
   
 };
 
-// Tiles length of each ship type, start and end coordinates of ship, boolean of whether it has been sunk
-const shipStatus = {
-  carrier: [[], 5],
-  battleship: [[], 4],
-  cruiser: [[],3],
-  submarine: [[], 3],
-  destroyer: [[], 2]
-}
 
 // Function to check if a ship is present at current coordinates
 const shipIsHere = function(currentBoard, coordinates) {
