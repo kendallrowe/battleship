51fda5d1e9c35213a48e1952521146baa60df1cb
Kendall Rowe <kendall.rowe312gmail.com>
2019-07-05 21:37:20 +0000

    Modularized and separated files. Changed to classes as opposed to objects to store board and ships

---
+++ public/constants.js
@@ -0,0 +1,7 @@
+const alphabetOrder = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+
+const convertCoordinates = function(coordinate) {
+
+};
+
+module.exports = { alphabetOrder, convertCoordinates };
---
+++ public/script.js
@@ -0,0 +1,146 @@
+const { alphabetOrder, convertCoordinates } = require("./constants"); 
+// Constructor function constaining closure function to generate spaces on the board
+class PlayerBoard {
+
+  constructor(playerNumber) {
+    this.playerNumber = playerNumber
+    this.playerFleet = new PlayerFleet();
+    this.createSpace = this.createSpace(-1, 1);
+    this.boardSpaces = {};
+    this.generateBoardSpaces();
+  }
+
+  // Create an empty board array for a player storing null ship type and shot fired within each cell
+  generateBoardSpaces() {
+
+    for (let i = 1; i <= 100; i++) {
+      this.boardSpaces[this.createSpace()] = new BoardSpace();
+    }
+
+  }
+
+  createSpace(alphaIndex, row) {
+  
+    return function() {
+      // console.log(alphaIndex);
+      // console.log(row);
+      if (alphaIndex === 9) {
+        row++;
+        alphaIndex = -1;
+      }
+      alphaIndex++;
+
+      return alphabetOrder[alphaIndex] + row;
+    };
+  }
+
+  fireShot(shotCoordinates) {
+    if (this.boardSpaces[shotCoordinates].hasBeenShot) {
+      return "Oops! Looks like you've already shot there. Try picking another spot.";
+    }
+
+    if (this.boardSpaces[shotCoordinates].shipOnSpace) {
+      this.boardSpaces[shotCoordinates].shipOnSpace.spacesHit.shipIsShot(shotCoordinates);
+
+    }
+  }
+
+}
+
+class BoardSpace {
+  constructor() {
+    this.hasBeenShot = false;
+  }
+}
+
+class PlayerFleet {
+  constructor() {
+    // this.generateShips();
+    this.shipsRemaining = 5;
+    this.carrier = new Carrier();
+    this.battleship = new Battleship();
+    this.cruiser = new Cruiser();
+    this.submarine = new Submarine();
+    this.destroyer = new Destroyer();
+  }
+  // // Create fleet of ships for a player and store in object
+  // generateShips() {
+  //   const shipTypes = ["carrier", "battleship", "cruiser", "submarine", "destroyer"];
+
+  //   for (let i = 0; i < shipTypes.length; i++) {
+  //     this[shipTypes[i]] = new Ship(shipTypes[i], this.player);
+  //   }
+  // }
+}
+
+class Ship {
+  constructor(maxHitAmount) {
+    this.spacesHit = []
+    this.maxHits = maxHitAmount;
+  }
+
+  shipIsShot(shotCoordinates) {
+    this.spacesHit.push(shotCoordinates);
+    if (this.spacesHit.length === this.maxHits) {
+
+    }
+  }
+
+  
+
+  placeShip(coordinates, orientation) {
+    for (let i = 1; i <= this.maxHits; i++) {
+
+    }
+    // coordinates.forEach(function(coordinate) {
+    //   playerBoard[coordinate].ship = ship;
+    // });
+  }
+}
+
+class Carrier extends Ship {
+  constructor() {
+    super(5);
+
+  }
+}
+
+class Battleship extends Ship {
+  constructor() {
+    super(4);
+  }
+}
+
+class Cruiser extends Ship {
+  constructor() {
+    super(3);
+  }
+}
+
+class Submarine extends Ship {
+  constructor() {
+    super(3);
+  }
+}
+
+class Destroyer extends Ship {
+  constructor() {
+    super(2);
+  }
+}
+
+
+
+const startNewGame = function() {
+
+  let playerOneBoard = new PlayerBoard("Player 1");
+  let playerTwoBoard = new PlayerBoard("Player 2");
+
+  // playerOneShips.placeShip("battleship", playerOneBoard, ["A1", "A2", "A3", "A4", "A5"]);
+
+  console.log(playerOneBoard);
+  console.log(playerTwoBoard);
+
+};
+
+const test = startNewGame();
